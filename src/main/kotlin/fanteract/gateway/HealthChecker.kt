40e2b60a6629943cfc26c38bce0d86c678ff47a9
Ho Sung <57245883+HoSungChoo@users.noreply.github.com>
2025-12-07 22:44:10 +0900

    인프라 구축 및 서비스 배포 (#4)
    인프라 구축 및 서비스 배포

---
+++ src/main/kotlin/fanteract/gateway/HealthChecker.kt
@@ -0,0 +1,13 @@
+package fanteract.gateway
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthChecker {
+    @GetMapping("/health")
+    fun healthCheck(): ResponseEntity<String>{
+        return ResponseEntity.ok().body("I am healthy !")
+    }
+}
