5d2fcecc364189b8fd3061dd4731f8eb9cbb210e
추호성 <hosung@chuhoseong-ui-MacBookPro.local>
2025-12-10 10:52:20 +0900

    FEAT

--- src/main/kotlin/fanteract/gateway/exception/ErrorResponse.kt
+++
@@ -1,9 +0,0 @@
-package fanteract.gateway.exception
-
-import java.time.Instant
-
-data class ErrorResponse(
-    val code: String,
-    val message: String,
-    val timestamp: Instant = Instant.now()
-)
